28c9f85c2f4d23fa43ee17083690d17cb579bd35
jjunior05 <jjquelito@gmail.com>
2020-10-30 21:12:23 -0300

    Ternary operator; enum and function as First Order Objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'input_dart.dart';
-
 import 'icon_content.dart';
+
+import 'input_dart.dart';
 import 'reusable_card.dart';
 
 const BottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1F33);
 const inactiveCardColour = Color(0xFF111328);
 const BottomContainerColor = Color(0xFFEB1555);
-
+enum Gender { male, female }
 void main() {
-  runApp(BmiCalculator());
+  runApp(BmiCalculator()); //Import input_card
 }
 
 class inputPage extends StatefulWidget {
@@ -21,27 +21,7 @@ class inputPage extends StatefulWidget {
 }
 
 class _inputPageState extends State<inputPage> {
-  Color maleCardColour = inactiveCardColour;
-  Color femaleCardColour = inactiveCardColour;
-
-  // 1 == male, 2 == female
-  void updateColour(int gender) {
-    if (gender == 1) {
-      if (maleCardColour == inactiveCardColour)
-        maleCardColour = activeCardColour;
-      else {
-        maleCardColour = inactiveCardColour;
-      }
-      femaleCardColour = inactiveCardColour;
-    } else {
-      if (femaleCardColour == inactiveCardColour)
-        femaleCardColour = activeCardColour;
-      else
-        femaleCardColour = inactiveCardColour;
-      maleCardColour = inactiveCardColour;
-    }
-  }
-
+  Gender selectedGender;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,34 +35,44 @@ class _inputPageState extends State<inputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  /** Class ReusableCard
+                   * Classe ReusableCard com os parâmetros (onPressed, colour,childCard)
+                   * **/
+                  child: ReusableCard(
+                    onPressed: () {
                       setState(() {
-                        updateColour(1);
+                        selectedGender = Gender.male;
                       });
                     },
-                    child: ReusableCard(
-                      colour: maleCardColour,
-                      childCard: IconContent(
-                        icon: FontAwesomeIcons.mars,
-                        label: "Male",
-                      ),
+                    /** Ternary operator para otimizar a escolha Female or Male através do Enum Gender
+                     * **/
+                    colour: selectedGender == Gender.male
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    childCard: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "Male",
                     ),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  /** Class ReusableCard
+                   * Classe ReusableCard com os parâmetros (onPressed, colour,childCard)
+                   * **/
+                  child: ReusableCard(
+                    onPressed: () {
                       setState(() {
-                        updateColour(2);
+                        selectedGender = Gender.female;
                       });
                     },
-                    child: ReusableCard(
-                      colour: femaleCardColour,
-                      childCard: IconContent(
-                        icon: FontAwesomeIcons.venus,
-                        label: "Female",
-                      ),
+                    /** Ternary operator para otimizar a escolha Female or Male através do Enum Gender
+                     * **/
+                    colour: selectedGender == Gender.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    childCard: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "Female",
                     ),
                   ),
                 ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final Widget childCard;
-  ReusableCard({@required this.colour, this.childCard});
+  final Function onPressed;
+  ReusableCard({@required this.colour, this.childCard, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: childCard,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: this.colour,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        child: childCard,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: this.colour,
+        ),
       ),
     );
   }
